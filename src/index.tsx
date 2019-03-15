e2bcd520d08338367da849b5f2f800590c0955e8
boushi-bird <boushi-bird@users.noreply.github.com>
2019-03-16 01:55:08 +0900

    font読み込み中に文字が出ない現象修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import './index.css';
+import WebFontLoader from 'webfontloader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import store from './store';
+
+WebFontLoader.load({
+  custom: {
+    families: ['Noto Sans Japanese:n1,n4,n7'],
+    urls: ['https://fonts.googleapis.com/earlyaccess/notosansjapanese.css'],
+  },
+});
 
 ReactDOM.render(
   <Provider store={store}>
