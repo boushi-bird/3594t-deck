fadaf73ce87a296fcc26e197034aaa6bff3c7ea5
boushi-bird <boushi-bird@users.noreply.github.com>
2020-10-08 23:21:00 +0900

    actionRedとactionBlueはシリアライズできないのでチェック対象から除外

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { datalistModule } from './modules/datalist';
 import { deckModule } from './modules/deck';
 import { dialogModule } from './modules/dialog';
 import { windowModule } from './modules/window';
+
+const middleware = getDefaultMiddleware({
+  serializableCheck: {
+    ignoredPaths: ['dialog.actionRed', 'dialog.actionBlue'],
+  },
+});
 
 const store = configureStore({
   reducer: {
@@ -11,6 +17,7 @@ const store = configureStore({
     dialog: dialogModule.reducer,
     window: windowModule.reducer,
   },
+  middleware,
 });
 
 export type State = ReturnType<typeof store.getState>;
